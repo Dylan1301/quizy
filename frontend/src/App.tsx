19c7a69ae57d770f65b809b5e40b6d7b6c2b1256
Hoa Nguyen (Rin) <thanhthanhhoa123@gmail.com>
2024-04-09 20:08:43 +1000

    Add Chakra UI components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,6 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <Button>Hello</Button>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
