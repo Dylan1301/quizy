8fb58d77be390f2d1e42c0a4792b03dcd9053d0b
Hoa Nguyen (Rin) <thanhthanhhoa123@gmail.com>
2024-04-10 14:22:16 +1000

    Add description to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h1 className="font-bold text-4xl">Quizy</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quas
+        enim id nulla temporibus deserunt ea possimus? Odio, iusto consequuntur!
+      </p>
       <UserPage />
     </>
   );
